f735929f8c06f61f951d9989d54cf85fa5a8034d
jade <jaytheminer7@gmail.com>
2026-02-22 09:20:36 +0900

    fix: 빌드테스트

--- packages/jady-js/src/index.ts
+++ packages/jady-js/src/index.ts
@@ -9,15 +9,33 @@ export interface JadyInstance {
   create(config?: Partial<JadyConfig>): JadyInstance;
 }
 
+function attachAliases<T>(res: JadyResponse<T>): JadyResponse<T> {
+  // axios 호환: res.data === res.body
+  if (!Object.getOwnPropertyDescriptor(res, "data")) {
+    Object.defineProperty(res, "data", {
+      enumerable: false,     // console.log에 안보이게
+      configurable: false,
+      get() {
+        return res.body;
+      }
+    });
+  }
+
+  return res;
+}
+
+
 export async function call<T = any>(config: JadyConfig): Promise<JadyResponse<T>> {
-  return dispatchRequest(config) as Promise<JadyResponse<T>>;
+  const res = await dispatchRequest(config) as JadyResponse<T>;
+  return attachAliases(res);
 }
 
 export function create(defaultConfig: Partial<JadyConfig> = {}): JadyInstance {
   const instance = async function(config: JadyConfig) {
-    const merged = mergeConfig(defaultConfig, config);
-    return dispatchRequest(merged);
-  } as JadyInstance;
+  const merged = mergeConfig(defaultConfig, config);
+  const res = await dispatchRequest(merged);
+  return attachAliases(res);
+} as JadyInstance;
 
   instance.create = (childConfig?: Partial<JadyConfig>) => create(mergeConfig(defaultConfig, childConfig || {}));
 
